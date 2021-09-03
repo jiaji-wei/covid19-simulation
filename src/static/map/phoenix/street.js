bc30dc6ffb6b66a960b9a02979ba2d4a2470afaf
AmagiDDmxh <amagiddmxh@qq.com>
2021-09-03 20:50:46 +0800

    Run lint fix

--- src/static/map/phoenix/street.js
+++ src/static/map/phoenix/street.js
@@ -1,3 +1,16 @@
-export var residential_streets = ["South Montezuma Avenue","South 5th Avenue","South 3rd Avenue","South 4th Avenue"];
-export var commercial_streets = ["West Hadley Street","South 2nd Avenue","West Sherman Street"];
-export var gathering_streets = ["South 1st Avenue","West Tonto Street","West Grant Street"];
+export var residential_streets = [
+  "South Montezuma Avenue",
+  "South 5th Avenue",
+  "South 3rd Avenue",
+  "South 4th Avenue",
+];
+export var commercial_streets = [
+  "West Hadley Street",
+  "South 2nd Avenue",
+  "West Sherman Street",
+];
+export var gathering_streets = [
+  "South 1st Avenue",
+  "West Tonto Street",
+  "West Grant Street",
+];
