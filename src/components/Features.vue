861b46a92af4362d92b41cd77cdac305fece2d69
AmagiDDmxh <amagiddmxh@qq.com>
2021-09-03 20:07:00 +0800

    Fix typos, add more descriptive texts

--- src/components/Features.vue
+++ src/components/Features.vue
@@ -4,8 +4,8 @@
     <div class="sm:w-3/4 lg:w-5/12 mx-auto px-2">
       <h1 class="text-3xl text-center text-bookmark-blue">Features</h1>
       <p class="text-center text-bookmark-grey mt-4">
-        Our aim is to make it quick and easy for you to simulat Covid-19
-         with different situation at different citys.
+        Our aim is to make it quick and easy for you to simulat Covid-19 with
+        different situation at different citys.
       </p>
     </div>
     <!-- Feature #1 -->
@@ -22,7 +22,13 @@
       >
         <div class="flex flex-1 justify-center z-10 mb-10 lg:mb-0">
           <img
-            class="w-5/6 h-5/6 sm:w-3/4 sm:h-3/4 md:w-full md:h-full rounded-3xl"
+            class="
+              w-5/6
+              h-5/6
+              sm:w-3/4 sm:h-3/4
+              md:w-full md:h-full
+              rounded-3xl
+            "
             src="./../imgs/tab1.png"
             alt=""
           />
@@ -40,12 +46,11 @@
               lg:w-full
             "
           >
-            You can choose any percentrage mask in use, such as surgical mask, N95, KN95 or cloth mask.
+            You can choose any percentrage mask in use, such as surgical mask,
+            N95, KN95 or cloth mask.
           </p>
-         
         </div>
       </div>
-      
     </div>
     <!-- Feature #2 -->
     <div class="relative mt-20 lg:mt-52">
@@ -62,14 +67,20 @@
         <!-- Image -->
         <div class="flex flex-1 justify-center z-10 mb-10 lg:mb-0">
           <img
-            class="w-5/6 h-5/6 sm:w-3/4 sm:h-3/4 md:w-full md:h-full rounded-3xl"
+            class="
+              w-5/6
+              h-5/6
+              sm:w-3/4 sm:h-3/4
+              md:w-full md:h-full
+              rounded-3xl
+            "
             src="./../imgs/tab2.png"
             alt=""
           />
         </div>
         <!-- Content -->
         <div class="flex flex-1 flex-col items-center lg:items-start">
-          <h1 class="text-3xl text-bookmark-blue">Public Polcy Simulat</h1>
+          <h1 class="text-3xl text-bookmark-blue">Public Policy Simulation</h1>
           <p
             class="
               text-bookmark-grey
@@ -80,12 +91,11 @@
               lg:w-full
             "
           >
-            Apply school closing, workplace closing or require all agents stay at home.
+            Apply school closing, workplace closing or require all agents stay
+            at home policies.
           </p>
-          
         </div>
       </div>
-      
     </div>
     <!-- Feature #3 -->
     <div class="relative mt-20 lg:mt-52">
@@ -102,14 +112,22 @@
         <!-- Image -->
         <div class="flex flex-1 justify-center z-10 mb-10 lg:mb-0">
           <img
-            class="w-5/6 h-5/6 sm:w-3/4 sm:h-3/4 md:w-full md:h-full rounded-3xl"
+            class="
+              w-5/6
+              h-5/6
+              sm:w-3/4 sm:h-3/4
+              md:w-full md:h-full
+              rounded-3xl
+            "
             src="./../imgs/tab3.png"
             alt=""
           />
         </div>
         <!-- Content -->
         <div class="flex flex-1 flex-col items-center lg:items-start">
-          <h1 class="text-3xl text-bookmark-blue">Commute in Different Environment</h1>
+          <h1 class="text-3xl text-bookmark-blue">
+            Commute in Different Environment
+          </h1>
           <p
             class="
               text-bookmark-grey
@@ -122,11 +140,9 @@
           >
             Agent can commute at home, workplace or other environment.
           </p>
-          
         </div>
       </div>
       <!-- Rounded Rectangle -->
-      
     </div>
   </section>
 </template>
